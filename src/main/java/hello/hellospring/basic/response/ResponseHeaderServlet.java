e41fd36c5d02b11e8d228bccb17c1547697cb27a
yesolje <yesolje@tigrison.com>
2024-01-30 23:01:02 +0900

    0130

---
+++ src/main/java/hello/hellospring/basic/response/ResponseHeaderServlet.java
@@ -0,0 +1,29 @@
+package hello.hellospring.basic.response;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "responseHeaderServlet" , urlPatterns = "/response-header")
+public class ResponseHeaderServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        //[status-line]
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+
+        //[response-headers]
+        response.setHeader("Content-Type", "text/plain;charset=utf-8");
+        response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        response.setHeader("Pragma", " no-cache");
+        response.setHeader("my-header", "good night");
+
+        PrintWriter writer = response.getWriter();
+        writer.println("OK");
+    }
+}
