f53ed5b1e42cec2b02fcd989418d05be8f62dd49
yesolje <yesolje@tigrison.com>
2024-02-10 00:24:42 +0900

    0210

---
+++ src/main/java/hello/hellospring/web/frontcontroller/v2/ControllerV2.java
@@ -0,0 +1,14 @@
+package hello.hellospring.web.frontcontroller.v2;
+
+import hello.hellospring.web.frontcontroller.MyView;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public interface ControllerV2 {
+
+    MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
+
+}
---
+++ src/main/java/hello/hellospring/web/frontcontroller/v2/controller/MemberListControllerV2.java
@@ -0,0 +1,25 @@
+package hello.hellospring.web.frontcontroller.v2.controller;
+
+import hello.hellospring.domain.member.Member;
+import hello.hellospring.domain.member.MemberRepository;
+import hello.hellospring.web.frontcontroller.MyView;
+import hello.hellospring.web.frontcontroller.v2.ControllerV2;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public class MemberListControllerV2 implements ControllerV2 {
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+    @Override
+    public MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            List<Member> members = memberRepository.findAll();
+
+            request.setAttribute("members", members);
+
+        return new MyView("/WEB-INF/views/members.jsp");
+    }
+}
